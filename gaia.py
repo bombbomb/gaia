3cb871ce7cab0b644ef123e692db9c58fa043de9
Patrick McDavid <pmcdavid@gmail.com>
2017-03-20 13:21:06 -0600

    sketching

--- gaia.py
+++ gaia.py
@@ -1,4 +1,4 @@
-import json
+import json, time, shutil, boto3
 
 from gaia.RegionRunner import RegionRunner
 
@@ -8,13 +8,141 @@ with open('config.json') as json_data:
     gaia_config = json.load(json_data)
     print(gaia_config)
 
+with open('aws.json') as json_data:
+    aws_config = json.load(json_data)
+    print("Got AWS creds")
+
+
+def create_version_zip():
+    gaia_config['version_num'] = str(round(time.time()))
+    gaia_config['zip_path'] = "build/version_" + gaia_config['version_num']
+    print("Zipping " + gaia_config['sourceCodePath'])
+    shutil.make_archive(gaia_config['zip_path'], 'zip', gaia_config['sourceCodePath'])
+    path_ = gaia_config['zip_path']
+    gaia_config['zip_path'] = path_ + '.zip'
+
+
+def get_iam_policy_doc():
+    return """{
+"Version": "2012-10-17",
+"Statement": [
+{
+  "Sid": "BucketAccess",
+  "Action": [
+    "s3:Get*",
+    "s3:List*",
+    "s3:PutObject"
+  ],
+  "Effect": "Allow",
+  "Resource": [
+    "arn:aws:s3:::elasticbeanstalk-*",
+    "arn:aws:s3:::elasticbeanstalk-*/*"
+  ]
+},
+{
+  "Sid": "XRayAccess",
+  "Action":[
+    "xray:PutTraceSegments",
+    "xray:PutTelemetryRecords"
+  ],
+  "Effect": "Allow",
+  "Resource": "*"
+},
+{
+  "Sid": "CloudWatchLogsAccess",
+  "Action": [
+    "logs:PutLogEvents",
+    "logs:CreateLogStream"
+  ],
+  "Effect": "Allow",
+  "Resource": [
+    "arn:aws:logs:*:*:log-group:/aws/elasticbeanstalk*"
+  ]
+}
+]
+}"""
+
+
+def create_iam_assets():
+    # create policy
+    iam = boto3.client('iam',
+                          aws_access_key_id=aws_config['key'],
+                          aws_secret_access_key=aws_config['secret'])
+
+    policy_name = "GaiaAppPolicy-" + gaia_config['appName']
+    path_prefix = '/GaiaApp/'
+    policy_list_response = iam.list_policies(PathPrefix=path_prefix)
+    policy_arn = None
+    for p in policy_list_response['Policies']:
+        if p['PolicyName'] == policy_name:
+            policy_arn = p['Arn']
+            print("Found policy")
+
+    if policy_arn is None:
+        create_policy_response = iam.create_policy(
+            PolicyName=policy_name,
+            Path=path_prefix,
+            PolicyDocument=get_iam_policy_doc()
+        )
+        policy_arn = create_policy_response['Policy']['Arn']
+
+    # create role attached to policy
+    role_name = "GaiaAppRole-" + gaia_config['appName']
+    role_list_response = iam.list_roles(PathPrefix=path_prefix)
+    role_arn = None
+
+    for r in role_list_response['Roles']:
+        if r['RoleName'] == role_name:
+            role_arn = r['Arn']
+            print("Found Role!")
+
+    if role_arn is None:
+        create_role_response = iam.create_role(
+            RoleName=role_name,
+            Path=path_prefix,
+            AssumeRolePolicyDocument="""{
+           "Version" : "2012-10-17",
+           "Statement": [ {
+              "Effect": "Allow",
+              "Principal": {
+                 "Service": [ "ec2.amazonaws.com" ]
+              },
+              "Action": [ "sts:AssumeRole" ]
+           }
+          ]
+        }"""
+        )
+        role_arn = create_role_response['Role']['Arn']
+
+    # create instance profile  create_instance_profile()
+    instance_profile_name = "GaiaAppInstanceProfile-" + gaia_config['appName']
+    try:
+        profile_response = iam.create_instance_profile(
+            InstanceProfileName=instance_profile_name,
+            Path=path_prefix
+        )
+        print("Instance Profile Created")
+    except Exception as e:
+        print("Instance Profile Already Exists")
+        pass
+
+    # attach role to policy   add_role_to_instance_profile()
+    try:
+        add_response = iam.add_role_to_instance_profile(
+            InstanceProfileName=instance_profile_name,
+            RoleName=role_name
+        )
+        print(add_response)
+    except Exception as e:
+        pass
+    gaia_config['instance_profile'] = instance_profile_name
+
+
+create_version_zip()
+create_iam_assets()
+
 # zone checking
 
 for region_name in gaia_config['regions']:
-    region_runner = RegionRunner(region_name, gaia_config)
-    region_runner.run()
-
-
-# check if EB App needs to be created
-
-# figure out current running version
+    region_runner = RegionRunner(region_name, gaia_config, aws_config)
+    region_runner.start()
--- gaia/RegionRunner.py
+++ gaia/RegionRunner.py
@@ -1,11 +1,110 @@
 import threading
+import boto3
 
 
 class RegionRunner(threading.Thread):
-    def __init__(self, region, config):
+    def __init__(self, region, config, aws_config):
         threading.Thread.__init__(self)
         self.region = region
         self.config = config
+        self.aws_config = aws_config
+
+        self.aws_eb = self.aws_client('elasticbeanstalk')
+        self.aws_s3 = self.aws_client('s3')
+        self.aws_iam = self.aws_client('iam')
 
     def run(self):
-        print("Ran " + self.region)
+        self.log("Started")
+        self.configure_elastic_beanstalk()
+
+    def configure_elastic_beanstalk(self):
+        self.log("Configuring Elastic Beanstalk")
+        apps = self.aws_eb.describe_applications(
+            ApplicationNames=[
+                self.config['appName'],
+            ]
+        )
+
+        self.log(apps)
+
+        if len(apps['Applications']) == 0:
+            self.create_eb_app()
+        else:
+            self.log("EB Application exists")
+
+        self.create_eb_version()
+        self.create_eb_environment()
+
+    def log(self, msg):
+        print("Region: " + self.region + ": ",  msg)
+
+    def create_eb_app(self):
+        self.log("Creating Elastic Beanstalk App")
+        self.aws_eb.create_application(
+            ApplicationName=self.config['appName']
+        )
+
+    def aws_client(self, client_name):
+        return boto3.client(client_name,
+                          aws_access_key_id=self.aws_config['key'],
+                          aws_secret_access_key=self.aws_config['secret'],
+                          region_name=self.region)
+
+    def create_eb_version(self):
+        bucket_name = "bb-gaia-versions-" + self.region + "-" + self.config['appName']
+        try:
+            self.aws_s3.head_bucket(Bucket=bucket_name)
+            self.log("Version bucket exists")
+        except:
+            location_contraint = {'LocationConstraint': self.region}
+
+            if self.region == 'us-east-1':
+                location_contraint = None
+
+            self.aws_s3.create_bucket(
+                Bucket=bucket_name,
+                CreateBucketConfiguration=location_contraint
+            )
+            self.log("Created Version Bucket")
+
+        self.log("Uploading Version to S3...")
+        version_key = 'version' + self.config['version_num'] + '.zip'
+        self.aws_s3.upload_file(
+            self.config['zip_path'],
+            bucket_name,
+            version_key
+        )
+
+        self.log("Creating Elastic Beanstalk Version")
+        self.aws_eb.create_application_version(
+            ApplicationName=self.config['appName'],
+            VersionLabel=self.config['version_num'],
+            SourceBundle={
+                'S3Bucket': bucket_name,
+                'S3Key': version_key
+            }
+        )
+        pass
+
+    def create_eb_environment(self):
+        self.log("Creating Elastic Beanstalk Environment")
+        version_num = 'version' + self.config['version_num']
+
+        option_settings = self.config['elasticBeanstalk']['optionSettings']
+        option_settings.append({
+            'Namespace': 'aws:autoscaling:launchconfiguration',
+            'OptionName': 'IamInstanceProfile',
+            'Value': self.config['instance_profile']
+        })
+
+        response = self.aws_eb.create_environment(
+            ApplicationName=self.config['appName'],
+            EnvironmentName=version_num,
+            CNAMEPrefix=self.config['appName'] + '-' + version_num,
+            VersionLabel=self.config['version_num'],
+            SolutionStackName=self.config['elasticBeanstalk']['solutionStack'],
+            OptionSettings=option_settings
+        )
+        self.log(response)
+
+        # poll for environment to go green
